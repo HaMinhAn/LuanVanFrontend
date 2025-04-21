a3d25027ff81de11b250c0f6b9165531de22f071
Ha Minh An <haminhan234@gmail.com>
2025-04-21 23:10:28 +0700

    update path for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+  base: "/", // important: set correct base path for Nginx/OpenShift
+  build: {
+    outDir: "dist", // default is 'dist'
+  },
+  server: {
+    port: 5173, // dev only
   },
 });
